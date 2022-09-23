d2bda47cc432b4c1df0bd8ef3182d13c475fafda
d-ries <swinnendries@gmail.com>
2022-09-23 14:36:16 +0200

    update to angular 14

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 
 
@@ -9,17 +9,17 @@ import { User } from 'src/app/models/user.model';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm!: FormGroup;
+  registerForm!: UntypedFormGroup;
   constructor() { }
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      username: new FormControl('',[Validators.required,Validators.minLength(3)]),
-      firstName: new FormControl('',[Validators.required]),
-      lastName: new FormControl('',[Validators.required]),
-      email: new FormControl('',[Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
-      age: new FormControl(null,[Validators.required]),
-      subscription: new FormControl('',[Validators.required])
+    this.registerForm = new UntypedFormGroup({
+      username: new UntypedFormControl('',[Validators.required,Validators.minLength(3)]),
+      firstName: new UntypedFormControl('',[Validators.required]),
+      lastName: new UntypedFormControl('',[Validators.required]),
+      email: new UntypedFormControl('',[Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+      age: new UntypedFormControl(null,[Validators.required]),
+      subscription: new UntypedFormControl('',[Validators.required])
     });
   }
 
